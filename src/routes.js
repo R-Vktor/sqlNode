e9fd39e57eb62e0a51ee87a125d72c528deb093a
Victor <rodrigueswp2@gmail.com>
2022-01-20 12:51:50 -0300

    paginacao de projetos

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,18 @@ const express = require('express');
 const routes = express.Router();
 
 const UserController = require('./controllers/UserController')
+const ProjectController = require('./controllers/ProjectController')
 
-routes.get('/users',UserController.index)
-routes.post('/users',UserController.create)
-routes.put('/users/:id',UserController.update)
-routes.delete('/users/:id',UserController.delete)
+
+routes
+    // recursos referentes a tabela users
+    .get('/users',UserController.index)
+    .post('/users',UserController.create)
+    .put('/users/:id',UserController.update)
+    .delete('/users/:id',UserController.delete)
+
+    //recursos referente a tabela projects
+    .get('/projects', ProjectController.index)
+    .post('/projects', ProjectController.create)
 
 module.exports = routes;
